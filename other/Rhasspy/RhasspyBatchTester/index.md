3a474495886d75168b8f209a20672ed7af35f581
KiboOst <KiboOst@users.noreply.github.com>
2020-01-30 21:23:09 +0100

    batcher

--- other/Rhasspy/RhasspyBatchTester/index.md
+++ other/Rhasspy/RhasspyBatchTester/index.md
@@ -1,6 +1,6 @@
 ---
 title: Rhasspy Assistant Tips n Tricks
-description: RhasspyBatchTester for Rhasspy Assistant.
+description: Rhasspy-BatchTester for Rhasspy Assistant.
 ---
 
 <img align="right" src="../images/rhasspyLogoLong.png" width="160" style="top: 15px">
@@ -11,13 +11,23 @@ description: RhasspyBatchTester for Rhasspy Assistant.
 
 Rhasspy-BatchTester is a python script that provide a way to mass test intents.
 
-You can test one or several sentences to intent recognition, or define a json with several sentences, intent that should be detected, and test it in one go with a report of matched / unmatched intents and there slots.
+You can test one or several sentences to Rhasspy intent recognition, or define a json file with several sentences and intent name that should match, and test it in one go.
 
 So, once you define new intents and re-train your rhasspy, just run the test to be sure it doesn't break other intents!
 
 ## Installation
 
-*Coming soon ...*
+Nothing fancy here, just a python file ...
+Download it on a PC or use wget to download it on your Rpi:
+
+```bash
+mkdir RhasspyBatchTester
+cd RhasspyBatchTester
+wget https://github.com/KiboOst/jeedom_docs/tree/master/other/Rhasspy/RhasspyBatchTester/pyRhasspyBatcher.py
+```
+
+[pyRhasspyBatcher.py](https://github.com/KiboOst/jeedom_docs/tree/master/other/Rhasspy/RhasspyBatchTester/pyRhasspyBatcher.py)
+
 
 ## Common usage
 
@@ -27,7 +37,7 @@ Here is the structure of such json file:
 
 ```json
 {
-	"allume la lumière de la cuisine": "lightsTurnOnJeedom",
+	"allume le champi d'axel pendant dix minutes": "TimeTurnOnJeedom",
 	"allume la lumière du canapé": "lightsTurnOnJeedom"
 }
 ```
@@ -58,6 +68,39 @@ Available arguments are:
 
 ## Python script usage
 
+You can write your own python script and import rhasspyBatcher as a python module. This allow you to set your own default settings and run the script whenever you need.
+
+<details>
+<summary>Here is an example of script:</summary>
+
+```python
+#!/usr/bin/env python3
+#-*- coding: UTF-8 -*-
+
+from argparse import Namespace
+from pyRhasspyBatcher import rhasspyBatcher
+
+if __name__ == "__main__":
+	args = Namespace(
+					addr="http://192.168.0.140",
+					port="12101",
+					json='',
+					sentence='',
+					intent='',
+					debug=0
+					)
+
+	rhasspy = rhasspyBatcher(args)
+
+	if rhasspy.connected:
+		rhasspy.testSentence("allume la lumière de la cuisine", "lightsTurnOnJeedom")
+		#rhasspy.testJsonFile("mySentences.json")
+	else:
+		print('Could not connect to Rhasspy')
+```
+
+</details>
+
 in Rhasspy-BatchTester folder you will find a python script example to test:
 
 `sudo nano /Rhasspy-BatchTester/runTest.py`
@@ -73,10 +116,22 @@ In this script, you can set your own arguments for later re-use, and what to do 
 Here is an example of what Rhasspy-BatchTester will output when testing:
 
 ```
-[ MATCHED] lightsTurnOnJeedom | query: allume la lumière de la cuisine | confidence:1.0 | Slots: house_room : cuisine
-[ MATCHED] turnOnJeedom | query: balance de la pop | confidence:1.0 | Slots: device_name : musique | music_genre : pop
-
-matched: 2 | unmatched: 0 | total: 2
+[     MATCHED] ShutterOpenJeedom | query: ouvre un peu le volet du salon | confidence:1.0 | Slots:  | house_room : salon | window_devices : volet | window_state : peu
+[     MATCHED] VolumeDownJeedom | query: baisse un peu le son | confidence:1.0 | Slots:  | ratio : peu
+[     MATCHED] TurnOnJeedom | query: lance la musique | confidence:1.0 | Slots:  | device_name : musique
+[     MATCHED] TurnOnJeedom | query: mets du jazz | confidence:1.0 | Slots:  | music_genre : Jazz
+[     MATCHED] TurnOffJeedom | query: éteins le chauffage dans la salle | confidence:1.0 | Slots:  | device_name : chauffage | house_room : salle
+[     MATCHED] ModeJeedom | query: passe les volets en manuel | confidence:1.0 | Slots:  | mode_name : volets | mode_type : manuel
+[     MATCHED] departJeedom | query: on y va | confidence:1.0 | Slots:  | depart : depart
+[     MATCHED] EntityStateValueJeedom | query: combien fait il dans la chambre d'axel | confidence:1.0 | Slots:  | device_name : temperature | house_room : axel
+[     MATCHED] EntityStateValueJeedom | query: C'est quoi cette chanson ? | confidence:1.0 | Slots:  | device_name : musique
+[     MATCHED] EntityStateValueJeedom | query: est ce que le volet de la chambre des parents est ouvert | confidence:1.0 | Slots:  | device_name : volet | house_room : parent
+[     MATCHED] EntityStateValueJeedom | query: combien fait il dehors | confidence:1.0 | Slots:  | device_name : temperature | house_room : dehors
+[     MATCHED] getWeather | query: quel temps il va faire demain | confidence:1.0 | Slots:  | when : forecast1
+[     MATCHED] TimeTurnOnJeedom | query: allume le champi d'axel pendant dix minutes | confidence:1.0 | Slots:  | device_name : champi_Axel | duration_mins : 10
+[     MATCHED] TimeTurnOffJeedom | query: laisse la cuisine éteinte pendant 25 minutes | confidence:1.0 | Slots:  | duration_mins : 25 | house_room : cuisine
+[     MATCHED] searchPreviousDate | query: c'était quand les dernières vacances | confidence:1.0 | Slots:  | annual_event : vacances
+matched: 22 | unmatched: 0 | total: 22
 ```
 
 Of course, *unmatched: 0* is what to achieve ;-)
---
+++ other/Rhasspy/RhasspyBatchTester/pyRhasspyBatcher.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+#-*- coding: UTF-8 -*-
+
+import sys, os
+import argparse
+import time
+import json
+import urllib
+
+import urllib.request
+requestUrl = urllib.request
+parseUrl = urllib.parse
+from http.cookiejar import CookieJar, LWPCookieJar
+
+try:
+	reload(sys)
+	sys.setdefaultencoding('utf-8')
+except:
+	pass
+
+class rhasspyBatcher():
+	def __init__(self, params):
+		self._version = 0.5
+
+		self.debugLevel = params.debug if params.debug > 0 else False
+
+		self.debug(3, '--addr: %s'%params.addr)
+		self.debug(3, '--port: %s'%params.port)
+		self.debug(3, '--json: %s'%params.json)
+		self.debug(3, '--sentence: %s'%params.sentence)
+		self.debug(3, '--intent: %s'%params.intent)
+		self.debug(3, '--debug: %s'%params.debug)
+
+		self.addr = params.addr
+		self.port = params.port
+
+		self._urlHost = self.addr+':'+self.port
+		self._reqHdl = None
+
+		self.matched = 0
+		self.unmatched = 0
+		self.tested = 0
+
+		self.connected = False
+		if self.connect() == True:
+			self.debug(1, "__Rhasspy connected__")
+			self.connected = True
+			if (params.json != ''):
+				self.testJsonFile(params.json)
+			if (params.sentence != ''):
+				self.testSentence(params.sentence, params.intent)
+	#
+	def connect(self):
+		answer = self.request('GET', self._urlHost, '/api/version')
+		self.debug(1, 'answer connect: %s'%answer)
+		if answer[0] == 200 : return True
+		return False
+	#
+	def testJsonFile(self, jsonPath, showResult=True):
+		if not os.path.exists(jsonPath):
+			return False
+
+		try:
+			batchJson = json.load(open(jsonPath, 'rb'), encoding='utf-8')
+			for query in batchJson:
+				self.debug(3, 'batch test: %s | %s'%(query, batchJson[query]))
+				try:
+					result = self.testQuery(query)
+					self.showResult(result, batchJson[query], query)
+				except Exception as e:
+					print('Batch test ERROR: ', e)
+					break
+				time.sleep(0.3)
+		except:
+			return False
+
+		if showResult:
+			self.showTotalResult()
+	#
+	def testSentence(self, query, matchIntent=''):
+		result = self.testQuery(query)
+		self.showResult(result, matchIntent, query)
+		time.sleep(0.3)
+	#
+
+
+	def testQuery(self, inputQuery):
+		inputQuery = inputQuery.lower()
+		answer = self.request('POST', self._urlHost, '/api/text-to-intent?nohass=true', inputQuery)
+		self.debug(6, "testQuery:answer %s"%answer)
+		return answer
+	#
+	def showResult(self, nluInference, matchIntent='', query='', showOnlyUnmatched=False):
+		self.tested += 1
+		displayResult = ''
+		showThis = True
+
+		intentInput = nluInference
+		intent = intentInput['intent']
+		confidence = intent['confidence']
+		intentName = intent['name']
+
+		entities = nluInference['entities']
+		slots = nluInference['slots']
+
+		#no intent found:
+		if intentName == '':
+			displayResult = '[UNFOUND     ]'
+			self.unmatched += 1
+			if matchIntent:
+				displayResult += ' should match: %s | query: %s'%(matchIntent, query)
+			print(displayResult)
+			return False
+
+		if matchIntent != '':
+			if matchIntent == intentName:
+				if showOnlyUnmatched: showThis = False
+				self.matched += 1
+				displayResult += '[     MATCHED] %s | query: %s'%(matchIntent, query)
+			else:
+				self.unmatched += 1
+				displayResult += '[---UNMATCHED] %s  should: %s | query: %s'%(intentName, matchIntent, query)
+
+		displayResult += ' | confidence:%s'%round(confidence, 2)
+
+		if showThis:
+			displaySlotResult = ''
+			for slot in slots:
+				slotName = slot
+				slotValue = slots[slot]
+				displaySlotResult += ' | %s : %s'%(slotName, slotValue)
+			if len(slots) == 0:
+				displaySlotResult = 'No slot found'
+			displayResult += ' | Slots: %s'%displaySlotResult
+			print(displayResult)
+	#
+	def showTotalResult(self):
+		print('matched: %s | unmatched: %s | total: %s'%(self.matched, self.unmatched, self.tested))
+	#
+	def request(self, method, host, path='', jsonString=None, postinfo=None): #standard function handling all get/post request
+		if self._reqHdl == None:
+			self._reqHdl = requestUrl.build_opener()
+			self._reqHdl.addheaders = [
+						('User-agent', 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:51.0) Gecko/20100101 Firefox/51.0'),
+						('Connection', 'keep-alive'),
+						('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8'),
+						('Upgrade-Insecure-Requests', 1)
+					]
+
+		url = host+path
+		self.debug(5, 'request: %s method: %s postinfo: %s'%(url, method, postinfo))
+
+		if method == 'GET':
+			try:
+				answer = self._reqHdl.open(url, timeout = 3)
+			except:
+				return ["404", None]
+		else:
+			if jsonString != None:
+				jsonBytes = jsonString.encode()
+				req = urllib.request.Request(url, data=jsonBytes, headers={'Content-Type': 'application/json'})
+				answer = self._reqHdl.open(req)
+
+			if postinfo != None:
+				data = parseUrl.urlencode(postinfo)
+				data.encode()
+				answer = self._reqHdl.open(url, data, timeout = 5)
+
+		if jsonString != None:
+			self.debug(5, 'request info: %s'%answer.info())
+			result = json.load(answer)
+			return result
+
+		return [answer.getcode(), answer.read()]
+	#
+	def debug(self, level, text):
+		if self.debugLevel >= level:
+			print("--debug:", text)
+	#
+	#
+#
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(prog='Rhasspy Batch Tester')
+
+	parser.add_argument('--addr', help='http://IP adress of Rhasspy', type=str)
+	parser.add_argument('--port', help='Port of Rhasspy', type=str, default='12101')
+	parser.add_argument('--json', help='json file path (see documentation)', type=str, default='')
+	parser.add_argument('--sentence', help='A sentence to test', type=str, default='')
+	parser.add_argument('--intent', help='The intent Name that should match --sentence', type=str, default='')
+	parser.add_argument('--debug', help='debug level', type=int, default=0)
+	args = parser.parse_args()
+
+	rhasspy = rhasspyBatcher(args)
